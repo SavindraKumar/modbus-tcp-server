1ed41933fdfbb91d2cf8dbe0c09d18ae47af5690
Savindra <savindran1989@gmail.com>
2019-01-25 20:42:58 +0530

    Enable only read input registers from modbus configuration

--- mbtcpconf.h
+++ mbtcpconf.h
@@ -20,14 +20,14 @@
 /******************************************************************************
  *                           Constants and typedefs
 ******************************************************************************/
-#define MBTP_CONF_FC_READ_COILS_ENABLE
-#define MBTP_CONF_FC_READ_DISCRETE_INPUTS_ENABLE
-#define MBTP_CONF_FC_READ_HOLDING_REGISTERS_ENABLE
-#define MBTP_CONF_FC_READ_INPUT_REGISTERS_ENABLE
-#define MBTP_CONF_FC_WRITE_COIL_ENABLE
-#define MBTP_CONF_FC_WRITE_HOLDING_REGISTER_ENABLE
-#define MBTP_CONF_FC_WRITE_COILS_ENABLE
-#define MBTP_CONF_FC_WRITE_HOLDING_REGISTERS_ENABLE
+#define MBTP_CONF_FC_READ_COILS_ENABLE               0
+#define MBTP_CONF_FC_READ_DISCRETE_INPUTS_ENABLE     0
+#define MBTP_CONF_FC_READ_HOLDING_REGISTERS_ENABLE   0
+#define MBTP_CONF_FC_READ_INPUT_REGISTERS_ENABLE     1
+#define MBTP_CONF_FC_WRITE_COIL_ENABLE               0
+#define MBTP_CONF_FC_WRITE_HOLDING_REGISTER_ENABLE   0
+#define MBTP_CONF_FC_WRITE_COILS_ENABLE              0
+#define MBTP_CONF_FC_WRITE_HOLDING_REGISTERS_ENABLE  0
 
 /******************************************************************************
  *                           Global variables
