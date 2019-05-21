66c31b11a8769caf5ea93b3ca9e9c417523a1ab2
Savindra <savindran1989@gmail.com>
2019-05-21 23:21:04 +0530

    Modify debug macros

--- src/mbap/mbap_debug.h
+++ src/mbap/mbap_debug.h
@@ -23,18 +23,22 @@
 //NOTE: Debug mask is chosen based on warning and msg level debug
 //      If warning leve and msg level debug changed mask should be changed
 #define MBT_CONF_DEBUG_MASK                         0x06
-#define MBT_CONF_DEBUG_WARNING_ENABLE               1
-#define MBT_CONFIG_DEBUG_MSG_ENABLE                 1
+#define MBT_DEBUG                                   1
+#define MBT_CONF_DEBUG_WARNING_ENABLE               0
+#define MBT_CONFIG_DEBUG_MSG_ENABLE                 0
 
 
 #if MBT_CONF_DEBUG_WARNING_ENABLE
 #define MBT_CONF_DEBUG_LEVEL_WARNING                0x02
+#else //MBT_CONF_DEBUG_WARNING_ENABLE
+#define MBT_CONF_DEBUG_LEVEL_WARNING                0x00
 #endif //MBT_CONF_DEBUG_WARNING_ENABLE
 
 #if MBT_CONFIG_DEBUG_MSG_ENABLE
 #define MBT_CONF_DEBUG_LEVEL_MSG                    0x04
+#else //MBT_CONF_DEBUG_DEBUG_ENABLE
+#define MBT_CONF_DEBUG_LEVEL_MSG                    0x00
 #endif //MBT_CONF_DEBUG_DEBUG_ENABLE
-
 
 
 #ifdef MBT_DEBUG
@@ -44,7 +48,7 @@
 #include <stdio.h>
 // print debug message only if debug message type is enabled
 #define MBT_DEBUGF(debug, message)    do { \
-                                             if (debug & DEBUG_MASK)\
+                                             if (debug & MBT_CONF_DEBUG_MASK)\
                                              {\
                                                  printf(message);\
                                              }\
