b168366f7dc8ed8f94df56c064cb6afe531cc1a6
Savindra Kumar <45175425+SavindraKumar@users.noreply.github.com>
2019-07-04 08:11:26 -0500

    Remove modbus data from main file
    This data is used mbap_user.c now which can be modify by user.

--- src/main.c
+++ src/main.c
@@ -19,21 +19,11 @@
 #include <string.h>
 //user defined files
 #include "mbap_conf.h"
-#include "tcp.h"
+#include "mbap_user.h"
 
 //****************************************************************************/
 //                           Defines and typedefs
 //****************************************************************************/
-#define INPUT_REGISTER_START_ADDRESS      (0u)
-#define MAX_INPUT_REGISTERS               (15u)
-#define HOLDING_REGISTER_START_ADDRESS    (0u)
-#define MAX_HOLDING_REGISTERS             (15u)
-#define DISCRETE_INPUTS_START_ADDRESS     (0u)
-#define MAX_DISCRETE_INPUTS               (3u)
-#define COILS_START_ADDRESS               (0u)
-#define MAX_COILS                         (3u)
-#define DISCRETE_INPUT_BUF_SIZE           (MAX_DISCRETE_INPUTS / 8u + 1u)
-#define COILS_BUF_SIZE                    (MAX_COILS / 8u + 1u)
 
 //****************************************************************************/
 //                           external variables
@@ -42,12 +32,7 @@
 //****************************************************************************/
 //                           Local variables
 //****************************************************************************/
-int16_t g_sInputRegsBuf[MAX_INPUT_REGISTERS]                = {1, 2, 3};
-int16_t g_sHoldingRegsBuf[MAX_HOLDING_REGISTERS]            = {5, 6, 7};
-uint8_t g_ucDiscreteInputsBuf[DISCRETE_INPUT_BUF_SIZE]      = {0xef};
-uint8_t g_ucCoilsBuf[COILS_BUF_SIZE]                        = {5};
-int16_t g_sHoldingRegsLowerLimitBuf[MAX_HOLDING_REGISTERS]  = {0, 0, 0};
-int16_t g_sHoldingRegsHigherLimitBuf[MAX_HOLDING_REGISTERS] = {200, 200, 200};
+
 //****************************************************************************/
 //                           Local Functions
 //****************************************************************************/
@@ -62,24 +47,7 @@ int16_t g_sHoldingRegsHigherLimitBuf[MAX_HOLDING_REGISTERS] = {200, 200, 200};
 //
 int main(void)
 {
-    ModbusData_t ModbusData;
-
-    ModbusData.psInputRegisters              = g_sInputRegsBuf;
-    ModbusData.usInputRegisterStartAddress   = INPUT_REGISTER_START_ADDRESS;
-    ModbusData.usMaxInputRegisters           = MAX_INPUT_REGISTERS;
-    ModbusData.psHoldingRegisters            = g_sHoldingRegsBuf;
-    ModbusData.usHoldingRegisterStartAddress = HOLDING_REGISTER_START_ADDRESS;
-    ModbusData.usMaxHoldingRegisters         = MAX_HOLDING_REGISTERS;
-    ModbusData.psHoldingRegisterLowerLimit   = g_sHoldingRegsLowerLimitBuf;
-    ModbusData.psHoldingRegisterHigherLimit  = g_sHoldingRegsHigherLimitBuf;
-    ModbusData.pucDiscreteInputs             = g_ucDiscreteInputsBuf;
-    ModbusData.usDiscreteInputStartAddress   = DISCRETE_INPUTS_START_ADDRESS;
-    ModbusData.usMaxDiscreteInputs           = MAX_DISCRETE_INPUTS;
-    ModbusData.pucCoils                      = g_ucCoilsBuf;
-    ModbusData.usCoilsStartAddress           = COILS_START_ADDRESS;
-    ModbusData.usMaxCoils                    = MAX_COILS;
-
-    mbap_DataInit(&ModbusData);
+    mu_Init();
     tcp_Init();
 
     return 0;
