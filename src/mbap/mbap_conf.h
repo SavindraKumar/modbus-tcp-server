99cd90f8658b2b845c10cc6363cf26a953dbb901
Savindra Kumar <45175425+SavindraKumar@users.noreply.github.com>
2019-07-04 08:13:26 -0500

    Use callback function in modbus data structure

--- src/mbap/mbap_conf.h
+++ src/mbap/mbap_conf.h
@@ -20,22 +20,40 @@
 //****************************************************************************
 //                           Constants and typedefs
 //****************************************************************************
-typedef struct
+typedef void(*pfnReadDiscreteInputs)(uint16_t usStartAddress,
+	                                 int16_t usNumOfData,
+	                                 uint8_t *pucRecBuf);
+
+typedef void(*pfnReadCoils)(uint16_t usStartAddress,
+	                        int16_t usNumOfData,
+	                        uint8_t *pucRecBuf);
+
+typedef void(*pfnReadInputRegisters)(uint16_t usStartAddress,
+                                     uint16_t usNumOfData,
+                                     uint8_t *pucRecBuf);
+
+typedef void(*pfnReadHoldingRegisters)(uint16_t usStartAddress,
+	                                   uint16_t usNumOfData,
+	                                   uint8_t *pucRecBuf);
+
+typedef void(*pfnWriteSingleHoldingRegister)(uint16_t usStartAddress,
+	                                         uint16_t usRegisterValue);
+typedef struct ModbusData
 {
-    int16_t *psInputRegisters;              //!< Pointer to Modbus Input Registers data
-    int16_t *psHoldingRegisters;            //!< Pointer to Modbus Holding Registers data
-    uint8_t *pucCoils;                      //!< Pointer to Modbus Coils Data
-    uint8_t *pucDiscreteInputs;             //!< Pointer to Modbus Discrete Inputs Data
-    int16_t *psHoldingRegisterLowerLimit;   //!< Pointer to Holding Register Lower Limits
-    int16_t *psHoldingRegisterHigherLimit;  //!< Pointer to Holding Register Lower Limits
-    uint16_t usInputRegisterStartAddress;   //!< Input Register Start Address
-    uint16_t usHoldingRegisterStartAddress; //!< Holding Register Start Address
-    uint16_t usCoilsStartAddress;           //!< Coil Start Address
-    uint16_t usDiscreteInputStartAddress;   //!< Discrete Input Start Address
-    uint16_t usMaxInputRegisters;           //!< Number of Input Registers
-    uint16_t usMaxHoldingRegisters;         //!< Number of Holding Registers
-    uint16_t usMaxCoils;                    //!< Number of Coils
-    uint16_t usMaxDiscreteInputs;           //!< Number of Discrete Inputs
+    int16_t                       *psHoldingRegisterLowerLimit;  //!<Pointer to Holding Register Lower Limits
+    int16_t                       *psHoldingRegisterHigherLimit; //!<Pointer to Holding Register Lower Limits
+    uint16_t                      usInputRegisterStartAddress;   //!<Input Register Start Address
+    uint16_t                      usHoldingRegisterStartAddress; //!<Holding Register Start Address
+    uint16_t                      usCoilsStartAddress;           //!<Coil Start Address
+    uint16_t                      usDiscreteInputStartAddress;   //!<Discrete Input Start Address
+    uint16_t                      usMaxInputRegisters;           //!<Number of Input Registers
+    uint16_t                      usMaxHoldingRegisters;         //!<Number of Holding Registers
+    uint16_t                      usMaxCoils;                    //!<Number of Coils
+    uint16_t                      usMaxDiscreteInputs;           //!<Number of Discrete Inputs
+    pfnReadInputRegisters         ptfnReadInputRegisters;        //!<Read Input Registers function
+	pfnReadHoldingRegisters       ptfnReadHoldingRegisters;      //!<Read Holding Registers function
+	pfnReadDiscreteInputs         ptfnReadDiscreteInputs;        //!<Read Discrete Inputs function
+	pfnReadCoils                  ptfnReadCoils;                 //!<Read Coils function
 } ModbusData_t;
 
 //! @brief Enable or Disable Read Coils  Function Code
@@ -51,16 +69,16 @@ typedef struct
 #define MBT_CONF_FC_READ_INPUT_REGISTERS_ENABLE     1
 
 //! @brief Enable or Disable Write Single Coil Function Code
-#define MBT_CONF_FC_WRITE_COIL_ENABLE               1
+#define MBT_CONF_FC_WRITE_COIL_ENABLE               0
 
 //! @brief Enable or Disable Write Single Holding Register Function Code
-#define MBT_CONF_FC_WRITE_HOLDING_REGISTER_ENABLE   1
+#define MBT_CONF_FC_WRITE_HOLDING_REGISTER_ENABLE   0
 
 //! @brief Enable or Disable Write Multiple Coil Function Code
-#define MBT_CONF_FC_WRITE_COILS_ENABLE              1
+#define MBT_CONF_FC_WRITE_COILS_ENABLE              0
 
 //! @brief Enable or Disable Write Single Holding Registers Function Code
-#define MBT_CONF_FC_WRITE_HOLDING_REGISTERS_ENABLE  1
+#define MBT_CONF_FC_WRITE_HOLDING_REGISTERS_ENABLE  0
 
 //****************************************************************************
 //                           Global variables
@@ -69,8 +87,21 @@ typedef struct
 //****************************************************************************
 //                           Global Functions
 //****************************************************************************
-void mbap_DataInit (const ModbusData_t *ModbusData);
-uint16_t mbap_ProcessRequest (const uint8_t *pucQuery, uint8_t ucQueryLen, uint8_t *pucResponse);
+//
+//! @brief Intialise Modbus Data
+//! @param[in]  ModbusData  Modbus data structure
+//! @return     None
+//
+void mbap_DataInit(const ModbusData_t *ModbusData);
+
+//
+//! @brief Process Modbus TCP Application request
+//! @param[in]   pucQuery      Pointer to Modbus TCP Query buffer
+//! @param[in]   ucQueryLen    Modbus TCP Query Length
+//! @param[out]  pucResponse   Pointer to Modbus TCP Response buffer
+//! @return      uint16_t      Modbus TCP Response Length
+//
+uint16_t mbap_ProcessRequest(const uint8_t *pucQuery, uint8_t ucQueryLen, uint8_t *pucResponse);
 
 
 #endif // MBAP_CONF_H
